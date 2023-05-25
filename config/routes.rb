9e78da7a6170ebc9efe3457059acc903275a05d1
Augusto Lopes <meleu.dev@gmail.com>
2023-05-25 18:49:54 -0300

    add routes/controllers/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :movies, only: %i[index show]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: %i[destroy]
 end
